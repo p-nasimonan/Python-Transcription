5710b21aa75b91b1109e352b4762499161d3bf6e
p-nasi <pnasimonan@gmail.com>
2024-11-19 09:45:27 +0900

    gitignore

--- analysis.py
+++ analysis.py
@@ -7,11 +7,10 @@ import whisper
 def calc(file):
     calc_device = "cpu"
     # Whisperモデルのロード
-    model = whisper.load_model("base", device=calc_device)  # 他に "tiny", "small", "medium", "large" も選択可能
+    model = whisper.load_model(name="base", device=calc_device)  # 他に "tiny", "small", "medium", "large" も選択可能
 
     # 音声ファイルの文字起こし
     result = model.transcribe(file)
 
-    # 文字起こし結果を表示
-    print(result['text'])
+    # 文字起こし結果を出力
     return result["text"]
